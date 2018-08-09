ca7eff35b2044f998af52b17995c96f5e677823b
Dhia <dhiasaadlaui@gmail.com>
2018-08-09 12:47:08 +0200

    Initialisation of all components that we will use to create the portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
+import { MiscellaneousComponent } from './miscellaneous/miscellaneous.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    EducationComponent,
+    SkillsComponent,
+    HobbiesComponent,
+    MiscellaneousComponent
   ],
   imports: [
     BrowserModule
