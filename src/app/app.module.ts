13acc2a92076b2b9d0823f21e455349e2c4da79f
unknown <dhiasaadlaui@gmail.com>
2020-08-05 02:23:32 +0100

    upgrade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProfileComponent } from './profile/profile.component';
-import { EducationComponent } from './education/education.component';
-import { SkillsComponent } from './skills/skills.component';
-import { HobbiesComponent } from './hobbies/hobbies.component';
-import { MiscellaneousComponent } from './miscellaneous/miscellaneous.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { RouterModule,Routes, Router} from '@angular/router'
-
-const appRoutes: Routes= [
-{path:'education',component:EducationComponent},
-{path:'hobbies',component:HobbiesComponent},
-{path:'miscellaneous',component:MiscellaneousComponent},
-{path:'skills',component:SkillsComponent},
-{path:'profile',component:ProfileComponent},
-{path:'',redirectTo:'/profile',pathMatch:'full'}
-];
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProfileComponent,
-    EducationComponent,
-    SkillsComponent,
-    HobbiesComponent,
-    MiscellaneousComponent,
-    NavbarComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,RouterModule.forRoot(appRoutes)
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
