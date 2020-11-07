52fc99b357c77589e23e8e7f3eabcdaf6c3902db
unknown <dhiasaadlaui@gmail.com>
2020-11-07 18:15:01 +0100

    voice reco

--- src/app/sections/resume/resume.component.ts
+++ src/app/sections/resume/resume.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import * as $ from 'jquery';
+
+
+
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
@@ -9,8 +12,21 @@ import * as $ from 'jquery';
 export class ResumeComponent implements OnInit {
   constructor(private route: ActivatedRoute, private router: Router) {}
 
-  ngOnInit(): void {}
+  language: string = "en-US";
 
+
+  ngOnInit(): void {
+    localStorage.setItem("lang","en-US");
+  }
+
+  setFR(){
+    localStorage.setItem("lang","fr-EN");
+    this.language = ('fr-EN')
+  }
+  setEN(){
+    localStorage.setItem("lang","en-US");
+    this.language = ('en-US')
+  }
   navigateTo(route) {
     this.router.navigate([route], { relativeTo: this.route });
   }
